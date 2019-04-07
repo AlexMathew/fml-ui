9c132784d175f570a2030d763c2e13c1cb214741
Alex Mathew <alexmathew003@gmail.com>
2019-04-07 10:45:42 +0530

    Move frontend to a separate repo

---
+++ build/precache-manifest.02c3af63947c875cf18d89b5a93d4256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfd00a8fa9aebe66132e",
+    "url": "/static/js/main.e8c9bed4.chunk.js"
+  },
+  {
+    "revision": "2ccc22200c374b844ae3",
+    "url": "/static/js/2.fa815246.chunk.js"
+  },
+  {
+    "revision": "cfd00a8fa9aebe66132e",
+    "url": "/static/css/main.f099f724.chunk.css"
+  },
+  {
+    "revision": "d227eeb19b7df68bc047b1566f5c6342",
+    "url": "/index.html"
+  }
+];
